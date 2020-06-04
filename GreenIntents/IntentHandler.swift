eebdc85bfc25df2a20ca0d4e941527dd39077627
Alessandro De Stefano <alexdsgm98@gmail.com>
2020-06-04 10:49:34 +0200

    Creata l'app extension e aggiungi gli intent da supportare

---
+++ GreenIntents/IntentHandler.swift
@@ -0,0 +1,20 @@
+//
+//  IntentHandler.swift
+//  GreenIntents
+//
+//  Created by Alessandro De Stefano on 04/06/2020.
+//  Copyright © 2020 Lorenzo Fasolino. All rights reserved.
+//
+
+import Intents
+
+class IntentHandler: INExtension {
+    
+    override func handler(for intent: INIntent) -> Any {
+        // This is the default implementation.  If you want different objects to handle different intents,
+        // you can override this and return the handler you want for that particular intent.
+        
+        return self
+    }
+    
+}
